908b4610a77ac8bb06939d65fa536354046954ec
Herman Sutanto <herman.sutanto@reebonz.com>
2017-08-11 14:04:48 +0800

    Routes & user accounts

--- client/main.js
+++ client/main.js
@@ -2,20 +2,46 @@ import { Meteor } from "meteor/meteor";
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Router, Route, browserHistory } from 'react-router';
+import { Tracker } from 'meteor/tracker';
 
 import Signup from '../imports/ui/Signup';
 import Login from '../imports/ui/Login';
 import Link from '../imports/ui/Link';
 import NotFound from '../imports/ui/NotFound';
 
+const unauthenticatedPages = ['/', '/signup'];
+const authenticatedPages = ['/links'];
+const onEnterPublicPage = () => {
+  if (Meteor.userId()) {
+    browserHistory.replace('/links');
+  }
+};
+const onEnterPrivatePage = () => {
+  if (!Meteor.userId()) {
+    browserHistory.replace('/');
+  }
+};
 const routes = (
   <Router history={browserHistory}>
-    <Route path="/" component={Login}/>
-    <Route path="/signup" component={Signup}/>
-    <Route path="/links" component={Link}/>
+    <Route path="/" component={Login} onEnter={onEnterPublicPage}/>
+    <Route path="/signup" component={Signup} onEnter={onEnterPublicPage}/>
+    <Route path="/links" component={Link} onEnter={onEnterPrivatePage}/>
     <Route path="*" component={NotFound}/>
   </Router>
 );
+
+Tracker.autorun(() => {
+  const isAuthenticated = !!Meteor.userId();
+  const pathname = browserHistory.getCurrentLocation().pathname;
+  const isUnauthenticatedPage = unauthenticatedPages.includes(pathname);
+  const isAuthenticatedPage = authenticatedPages.includes(pathname);
+
+  if (isUnauthenticatedPage && isAuthenticated) {
+    browserHistory.replace('/links');
+  } else if (isAuthenticatedPage && !isAuthenticated) {
+    browserHistory.replace('/');
+  }
+});
 
 Meteor.startup(() => {
   ReactDOM.render(routes, document.getElementById('app'));
