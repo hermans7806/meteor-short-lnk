0b81d82940d5cce45f951456fc12dce043b4c460
Herman <herman.sutanto@reebonz.com>
2017-08-16 07:11:44 +0800

    toggle to show hidden links

--- client/main.js
+++ client/main.js
@@ -1,6 +1,7 @@
 import { Meteor } from "meteor/meteor";
 import ReactDOM from 'react-dom';
 import { Tracker } from 'meteor/tracker';
+import { Session } from 'meteor/session';
 
 import {routes, onAuthChange} from '../imports/routes/routes';
 import '../imports/startup/simple-schema-configuration';
@@ -10,16 +11,7 @@ Tracker.autorun(() => {
   onAuthChange(isAuthenticated);
 });
 
-// // Stateless functional components
-// import React from 'react';
-// const MyComponent = (props) => {
-//   return (
-//     <div>
-//       <h1>MyComponent is here! {props.name}</h1>
-//     </div>
-//   );
-// };
-
 Meteor.startup(() => {
+  Session.set('showVisible', true);
   ReactDOM.render(routes, document.getElementById('app'));
 });
--- imports/ui/LinksListItem.js
+++ imports/ui/LinksListItem.js
@@ -1,13 +1,21 @@
+import { Meteor } from 'meteor/meteor';
 import React from 'react';
 import PropTypes from 'prop-types';
 import Clipboard from 'clipboard';
 
 export default class LinksListItem extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      justCopied: false
+    };
+  }
   componentDidMount() {
     this.clipboard = new Clipboard(this.refs.copy);
 
     this.clipboard.on('success', () => {
-      alert('It Worked!');
+      this.setState({ justCopied: true });
+      justCopied = setTimeout(() => this.setState({ justCopied: false }), 1000);
     }).on('error', () => {
       alert('Unable to copy. Please manually copy the link.')
     });
@@ -20,7 +28,15 @@ export default class LinksListItem extends React.Component {
       <div>
         <p>{this.props.url}</p>
         <p>{this.props.shortUrl}</p>
-        <button ref="copy" data-clipboard-text={this.props.shortUrl}>Copy</button>
+        <p>{this.props.visible.toString()}</p>
+        <button ref="copy" data-clipboard-text={this.props.shortUrl}>
+          {this.state.justCopied ? 'Copied' : 'Copy'}
+        </button>
+        <button onClick={() => {
+          Meteor.call('links.setVisibility', this.props._id, !this.props.visible);
+        }}>
+          {this.props.visible? 'Hide' : 'Unhide'}
+        </button>
       </div>
     );
   }
@@ -30,5 +46,6 @@ LinksListItem.propTypes = {
   url: PropTypes.string.isRequired,
   shortUrl: PropTypes.string.isRequired,
   _id: PropTypes.string.isRequired,
-  userId: PropTypes.string.isRequired
+  userId: PropTypes.string.isRequired,
+  visible: PropTypes.bool.isRequired
 };
